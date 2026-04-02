f3c4f4b001a96e0e04f3b808b2de2d3f6a0c7ce1
Markus Olsen <markusol@hiof.no>
2026-04-02 21:40:36 +0200

    Flyttet filer fra gammel branch til Specific-case-page

---
+++ finestra/src/Components/CaseEventCard/CaseEventCard.tsx
@@ -0,0 +1,9 @@
+import "./CaseEventCard.css";
+export default function CaseEventCard() {
+  return (
+    <div className="case-event-card">
+      <h3>description</h3>
+      <p>created_at</p>
+    </div>
+  );
+}
